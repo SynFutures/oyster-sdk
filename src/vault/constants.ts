919a647320ce9afaf2fe136069f1dea02ec29846
allan-wei-tx <1370485525@qq.com>
2024-07-22 16:57:12 +0800

    Update vaultFactory address

--- src/vault/constants.ts
+++ src/vault/constants.ts
@@ -1,5 +1,5 @@
 import { CHAIN_ID } from '@derivation-tech/web3-core';
 
 export const VAULT_FACTORY_ADDRESSES: { [chainId: number]: string } = {
-    [CHAIN_ID.BASE]: '0x138d60aDdB5046B7bb45f81ad5554fA436CAbAB5',
+    [CHAIN_ID.BASE]: '0xEb8F6266Ab36A296fF8D092E45D2327511FB7bA6',
 };
