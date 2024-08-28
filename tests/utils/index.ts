a5e970c25049b344366d6ff8aa4e381a53471aab
liulinhui <1249055292@qq.com>
2024-08-28 09:29:24 +0800

    update liquidity test

--- tests/utils/index.ts
+++ tests/utils/index.ts
@@ -1,1 +1,14 @@
+import { BigNumber } from 'ethers';
+import _ from 'lodash';
+
 export * from './ganache';
+export function addedDeadline(deadline: number): number {
+    return Math.round(
+        Number(
+            BigNumber.from(_.now())
+                .div(1000)
+                .add(deadline * 60)
+                .toString(),
+        ),
+    );
+}
